a1dac471dac55ae02c53b3701704f1ee7500fb38
dlee67 <dlee67@msudenver.edu>
2018-12-23 22:54:07 -0700

    Moar spaghetti.

---
+++ AboutUtilityClasses/CoursesContract.java
@@ -0,0 +1,13 @@
+/*
+  Contract classes are utility classes in nature.
+*/
+
+public class CoursesContract {
+
+    // The below prevents an instantiation, which further encapsulates
+    // the content of Android's ContractClass.
+    private CoursesContract(){
+        throw new UnsupportedOperationException();
+    }
+
+}
