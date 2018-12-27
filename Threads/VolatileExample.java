79989bf2114eeb3f7c49973062481dac75a5aa1e
dlee67 <dlee67@msudenver.edu>
2018-12-26 17:03:56 -0700

    Bunch of code those aren't mine. The directories represent the books I've copy pasted codebase from.

--- Threads/VolatileExample.java
+++ Threads/VolatileExample.java
@@ -1,6 +1,6 @@
 public class VolatileExample {
-    // Keyword volatile enables all the Threads to see the declared variable.
-    // https://examples.javacodegeeks.com/core-java/java-volatile-keyword-example/
+    // Variable marked with volatile behaves as if every reference to it were
+    // wrapped in a synchronization block.
     static volatile boolean stop;
 
     private static final Runnable job = new Runnable() {
